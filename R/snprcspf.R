2ce3bda68e135d8e97ac2d98b52e72b629519a98
R. Mark Sharp <msharp@TxBiomed.org>
2017-05-23 16:54:49 -0500

    Added code to report on samples with missing dates. It may not be robust as it needs to have the string "CNT" in the control samples to not report those as well.

--- R/snprcspf.R
+++ R/snprcspf.R
@@ -1452,6 +1452,13 @@ get_date_from_sample <- function(conn, samples) {
                 get_and_or_list(samples[date_not_id & id_not_id])))
   possible$dates[id_not_id] <- possible$ids[id_not_id]
   possible$dates[stri_length(possible$dates) < 8] <- NA
+  if (any(is.na(possible$dates)) &
+      any(!stri_detect_fixed(samples[is.na(possible$dates)], "CNT"))) {
+    warning(stri_c(
+      "The following sample labels do not have recognizable dates: ",
+      get_and_or_list(samples[is.na(possible$dates) &
+                                !stri_detect_fixed(samples, "CNT")])))
+  }
   possible$dates
 }
 #' Returns character vector with the raw data (raw mfi) table
