2abf901f977a3b48bfe37d34654eb80d086fd26d
R. Mark Sharp <msharp@TxBiomed.org>
2017-07-14 17:13:53 -0500

    Starting to add code to read agents and antigens from list in LabKey.

--- R/snprcspf.R
+++ R/snprcspf.R
@@ -922,8 +922,9 @@ get_agents_and_antigen_pairs <- function() {
              a2 = c("STLV p21", "SRV gp-20", "HVP-2", "HVP-2",
                     "SIV mac", "SIV mac", "rMeasles", "rChagas"),
              stringsAsFactors = FALSE)
+
   # labkey.selectRows(
-  #   baseUrl = "https://vger.txbiomed.org/labkey",
+  #   baseUrl = attr(session, "baseUrl"),
   #   folderPath = "/SNPRC/Core Facilities/SPF Screen Workflow",
   #   schemaName = "lists",
   #   queryName = "agent_antigen_pairs",
@@ -932,6 +933,19 @@ get_agents_and_antigen_pairs <- function() {
   #   containerFilter = NULL, colNameOpt = "fieldname"
   # )
 }
+#' #' Makes function to return agents and antigen pairs.
+#' #'
+#' #' @param session session object from LabKey.
+#' #' @import Rlabkey
+#' make_get_agents_and_antigen_pairs <- function(session) {
+#'   schema_conn <- getSchema(session, "lists")
+#'   schema_element <- schema_conn[["agent_antigen_pairs"]]
+#'   agents_and_antigen_pairs <- getRows(session, query)
+#'   f <- function() {
+#'     agents_and_antigen_pairs
+#'   }
+#' }
+#'
 #' Returns the list of test antigens
 #'
 #' Uses \code{get_agents_and_antigen_pairs()} to create the list of unique
