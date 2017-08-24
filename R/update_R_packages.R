b60195fc6de458a0d38377df9eec9c06eb0ad517
R. Mark Sharp <msharp@TxBiomed.org>
2017-08-24 16:44:43 -0500

    Refactored to comment functions, rename functions, and create functions.

--- R/update_R_packages.R
+++ R/update_R_packages.R
@@ -2,13 +2,22 @@
 ### Updates R after new installation of R
 ##
 ## This script will use Sys.info() to discover what system it is running on
-## and assign the source_path and lib_path values. However, command line
-## arguments can be used to override these using a command such as the
-## following:
+## and assign the source_path, custom_lib_path, and install_path values.
+## However, command line arguments can be used to override these using a
+## command such as the following:
 ## RScript update_R_packages.R "/Users/msharp/Documents/Development/R/r_workspace/library/" "/Library/Frameworks/R.framework/Versions/3.4/Resources/library/"
+## This script is written assuming that only base and default packages are
+## available.
+##
+## Function definitions are written using roxygen2 comments so that they could
+## be lifted and put into a package as they are.
+## Function definitions:
+##
 #' Get package dependencies
 #'
 #' Written by Josh O'Brien on stackoverflow on May 13 '15 at 21:42
+#' @param path character vector of length one having the directory path to
+#' where new version of the custom package exists.
 get_deps <- function(path) {
   dcf <- read.dcf(file.path(path, "DESCRIPTION"))
   jj <- intersect(c("Depends", "Imports", "Suggests"), colnames(dcf))
@@ -16,11 +25,35 @@ get_deps <- function(path) {
   val <- gsub("\\s.*", "", trimws(val))
   val[val != "R"]
 }
+#' Make dependency list
+#'
+#' Gathers the dependencies from each source package and combines the lists
+#' without duplication.
+#'
+#' @param source_names character vector containing the names of the custom
+#' packages to be installed from source.
+#' @param path character vector of length one having the path to R's
+#' \code{library} directory, which contains the packages being updated from
+#' source. This may or may or may not be the same as the system wide
+#' \code{library} directory. It could be a user directory.
+make_dependency_list <- function(source_names, path) {
+  dependencies <- character(0)
+  for (name in souce_names) {
+    dependencies <- unique(c(dependencies, get_deps(paste0(path, name))))
+  }
+  dependencies
+}
 #' Removed these strings
 #'
 #' Modified from rmsutilityr::remove_strings() by R. Mark Sharp. The
 #' modification was to remove a package dependency using the standard
 #' relational opporator "==" instead of stri_detect_regex().
+#' @param .str character vector that have tokens removed that match
+#' tokens within the \code{expunge} vector.
+#' @param expunge character vector of tokens to be removed from the
+#' \code{.str} vector if present.
+#' @param ignore_case boolean that determines whether or not case is ignored.
+#' Defaults to FALSE.
 remove_these_str <- function(.str, expunge, ignore_case = FALSE) {
   if (ignore_case) {
     tmp_str <- tolower(.str)
@@ -36,53 +69,60 @@ remove_these_str <- function(.str, expunge, ignore_case = FALSE) {
   }
   .str[keep]
 }
-info <- Sys.info()
-if (toupper(info$nodename) == "VGER" | toupper(info$nodename) == "BOOMER") {
-  source_path <- "d:Labkey data/"
-  lib_path <- "c:/R Library/"
-} else {
-  lib_path <- "/Library/Frameworks/R.framework/Versions/3.4/Resources/library/"
-  source_path <- "/Users/msharp/Documents/Development/R/r_workspace/library/"
-}
-args <- commandArgs(trailingOnly = TRUE)
-if (length(args) == 2) {
-  source_path <- args[1]
-  lib_path <- args[2]
-  if (!length(list.files(path = source_path, pattern = ".*tar.gz")) > 0)
-    stop(paste0(source_path, " does not contain any package sources. ",
-                "I am assuming the path is incorrect"))
-  if (!file.exists(paste0(lib_path, "utils")))
-    stop(paste0(lib_path, " does not contain required R package 'utils'. ",
-                "I am assuming the path is incorrect."))
-}
-
-## dependencies becomes the collection of packages needed to support our
-## custom packages
-dependencies <- get_deps(paste0(lib_path, "rmsutilityr"))
-dependencies <- unique(c(dependencies, get_deps(paste0(lib_path, "animalr"))))
-dependencies <- unique(c(dependencies, get_deps(paste0(lib_path, "snprcspf"))))
-dependencies <- unique(c(dependencies,
-                              get_deps(paste0(lib_path,
-                                              "renameSurgerySheets"))))
-## Remove base packages and local custom packages from dependencies
-remove_these <- c("stats", "tools", "utils", "rmsutilityr", "animalr",
-                  "renameSurgerySheets")
-dependencies <- remove_these_str(dependencies, expunge = remove_these)
-
-install.packages(dependencies, type = "binary",
-                 repos = "https://cran.revolutionanalytics.com",
-                 lib = lib_path)
-update.packages(lib.loc = lib_path,
-                repos = "https://cran.revolutionanalytics.com", ask = FALSE)
-
-update_from_source <- function(source_names, source_path, lib_path) {
+#' Install R package from package source
+#'
+#' Takes a list of packages (\code{source_names}) and the path
+#' (\code{source_path}) to their common location and
+#' installs them into the \code{install_path} directory.
+#'
+#' @param source_names character vector having one source package name per
+#' cell.
+#' @param source_path
+install_from_source <- function(source_names, source_path, install_path) {
   for (source_name in source_names) {
     source <- max(list.files(path = source_path,
                              pattern = paste0(source_name, ".*.tar.gz")))
     install.packages(paste0(source_path, source), type = "source", repos = NULL,
-                     lib = lib_path)
-    }
+                     lib = install_path)
+  }
 }
-source_names <- c("rmsutilityr", "animalr", "renameSurgerySheets")
-update_from_source(source_names = source_names,
-                   source_path = source_path, lib_path = lib_path)
+# ## end of function definitions
+# source_names <- c("rmsutilityr", "animalr", "snprcspf", "snprcpath",
+#                   "renameSurgerySheets")
+#
+# node_name <- Sys.info()[["nodename"]]
+# if (toupper(node_name) == "VGER" | toupper(node_name) == "BOOMER") {
+#   source_path <- "d:Labkey data/"
+#   install_path <- "c:/R Library/"
+# } else {
+#   install_path <-
+#     "/Library/Frameworks/R.framework/Versions/3.4/Resources/library/"
+#   source_path <- "/Users/msharp/Documents/Development/R/r_workspace/library/"
+# }
+# args <- commandArgs(trailingOnly = TRUE)
+# if (length(args) == 2) {
+#   source_path <- args[1]
+#   install_path <- args[2]
+#   if (!length(list.files(path = source_path, pattern = ".*tar.gz")) > 0)
+#     stop(paste0(source_path, " does not contain any package sources. ",
+#                 "I am assuming the path is incorrect"))
+#   if (!file.exists(paste0(install_path, "utils")))
+#     stop(paste0(install_path, " does not contain required R package 'utils'. ",
+#                 "I am assuming the path is incorrect."))
+# }
+#
+# dependencies <- make_dependency_list(source_names, install_path)
+#
+# ## Remove base packages and local custom packages that are to be installed
+# ## from the vector of dependencies.
+# remove_these <- c("stats", "tools", "utils", source_names)
+# dependencies <- remove_these_str(dependencies, expunge = remove_these)
+#
+# install.packages(dependencies, type = "binary",
+#                  repos = "https://cran.revolutionanalytics.com",
+#                  lib = install_path)
+# update.packages(lib.loc = install_path,
+#                 repos = "https://cran.revolutionanalytics.com", ask = FALSE)
+#
+# install_from_source(source_names = source_names,
+#                    source_path = source_path, install_path = install_path)
