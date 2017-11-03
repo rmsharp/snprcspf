8aca9a7603f1b52bac72bfe857bb58b05929b677
rmsharp <rmsharp@me.com>
2017-11-02 20:50:52 -0500

    Removed lintr tests on travis and cran. It seems my lintrs are not being used. Removed old comments in code replaced by information in README

--- tests/testthat/test_with_lintr.R
+++ tests/testthat/test_with_lintr.R
@@ -1,6 +1,8 @@
 library(snprcspf)
 context("Running lintr on code")
 if (requireNamespace("lintr", quietly = TRUE)) {
+  skip_on_cran()
+  skip_on_travis()
   context("lints")
   test_that("Package Style", {
     lintr::expect_lint_free()
