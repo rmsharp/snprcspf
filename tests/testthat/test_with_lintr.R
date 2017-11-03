e3a5635201e9c7cb9d6c40667931b0be26b09baf
rmsharp <rmsharp@me.com>
2017-11-02 21:13:38 -0500

    moved test for travis

--- tests/testthat/test_with_lintr.R
+++ tests/testthat/test_with_lintr.R
@@ -1,10 +1,10 @@
 library(snprcspf)
 context("Running lintr on code")
 if (requireNamespace("lintr", quietly = TRUE)) {
-  skip_on_cran()
-  skip_on_travis()
   context("lints")
   test_that("Package Style", {
+    skip_on_cran()
+    skip_on_travis()
     lintr::expect_lint_free()
   })
 }
